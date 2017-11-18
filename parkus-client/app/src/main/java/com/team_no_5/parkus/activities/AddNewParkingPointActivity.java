0fa468cdbcb026619b395a6e2b9e0277eb8194e8
Michał Zawadzki <michalzaw30@gmail.com>
2017-11-18 21:12:50 +0100

    Moved function 'getCurrentLocation' to 'AddNewParkingPoitActivity'.

--- parkus-client/app/src/main/java/com/team_no_5/parkus/activities/AddNewParkingPointActivity.java
+++ parkus-client/app/src/main/java/com/team_no_5/parkus/activities/AddNewParkingPointActivity.java
@@ -1,5 +1,11 @@
 package com.team_no_5.parkus.activities;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +17,40 @@ public class AddNewParkingPointActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_parking_point);
+    }
+
+    private static void getCurrentLoaction(AppCompatActivity activity) {
+        LocationManager locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                locationManager.removeUpdates(this);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 }
--- parkus-client/app/src/main/java/com/team_no_5/parkus/activities/MainMapActivity.java
+++ parkus-client/app/src/main/java/com/team_no_5/parkus/activities/MainMapActivity.java
@@ -1,9 +1,6 @@
 package com.team_no_5.parkus.activities;
 
-import android.location.Location;
-import android.location.LocationListener;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,15 +13,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.team_no_5.parkus.R;
-import com.team_no_5.parkus.Utilities.AdvancedCallable;
 import com.team_no_5.parkus.Utilities.Locations;
 import com.team_no_5.parkus.adapters.PointInfoAdapter;
 import com.team_no_5.parkus.networking.ParkingPointsNetworking;
 import com.team_no_5.parkus.networking.items.ParkingPoint;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -69,36 +63,6 @@ public class MainMapActivity extends AppCompatActivity implements OnMapReadyCall
         parkingPointsNetworking.loadParkingPoints(
                 () -> {
                     parkingPoints = parkingPointsNetworking.getParkingPoints();
-
-                    Locations.getCurrentLoaction(this, new LocationListener() {
-                        @Override
-                        public void onLocationChanged(Location location) {
-                            ParkingPoint p = new ParkingPoint();
-                            p.setLatitude(location.getLatitude());
-                            p.setLongitude(location.getLatitude());
-
-                            parkingPoints.add(p);
-
-                            LatLng coords = new LatLng(p.getLatitude(), p.getLongitude());
-                            Marker marker = map.addMarker(new MarkerOptions().position(coords));
-                            marker.setTag(p);
-                        }
-
-                        @Override
-                        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-                        }
-
-                        @Override
-                        public void onProviderEnabled(String s) {
-
-                        }
-
-                        @Override
-                        public void onProviderDisabled(String s) {
-
-                        }
-                    });
 
                     for (ParkingPoint p : parkingPoints) {
                         LatLng coords = new LatLng(p.getLatitude(), p.getLongitude());
