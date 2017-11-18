8d60b48207bc08ead55b941cb7b1c5aa5349a314
Jakub Biliński <jbilinskipl@gmail.com>
2017-11-18 15:42:18 +0100

    Added user item.

---
+++ parkus-server/parkus-server/Models/UserItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace parkus_server.Models
+{
+    public class UserItem
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Points { get; set; }
+    }
+}
