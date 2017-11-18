583c56f3730845418fe561632d4685b124f87f67
Jakub Biliński <jbilinskipl@gmail.com>
2017-11-18 12:07:59 +0100

    Added empty Android Project.

---
+++ parkus-client/app/src/main/java/com/team_no_5/parkus/BeginActivity.java
@@ -0,0 +1,13 @@
+package com.team_no_5.parkus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin);
+    }
+}
