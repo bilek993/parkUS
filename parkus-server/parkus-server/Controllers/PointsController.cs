1927de64e0cdf2f4e59ad43bc8a11fe1e3172603
Jakub Biliński <jbilinskipl@gmail.com>
2017-11-19 02:19:12 +0100

    Added controller with Get for points.

---
+++ parkus-server/parkus-server/Controllers/PointsController.cs
@@ -0,0 +1,25 @@
+﻿using parkus_server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace parkus_server.Controllers
+{
+    public class PointsController : ApiController
+    {
+        [BasicAuthentication]
+        public int Get()
+        {
+            using (var database = new DatabaseMainEntities())
+            {
+                string username = Thread.CurrentPrincipal.Identity.Name;
+                User user = database.User.FirstOrDefault(u => u.Username == username);
+                return user.Points ?? 0;
+            }
+        }
+    }
+}
